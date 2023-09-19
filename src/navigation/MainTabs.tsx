4c52fe087b064f4e99d79ae08c6c3601d3219588
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-09-19 17:10:39 -0300

    adding icons to bottom navigation

--- src/navigation/MainTabs.tsx
+++ src/navigation/MainTabs.tsx
@@ -1,28 +1,37 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MainTabsParamList } from "./types";
-import { useTheme } from "../state/theme";
 import { NavigationContainer } from "@react-navigation/native";
 import ExploreScreen from "../screens/main/Explore";
 import SearchScreen from "../screens/main/Search";
+import AdvertiseScreen from "../screens/main/Advertise";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const MainTabs = createBottomTabNavigator<MainTabsParamList>();
 
 export default function MainTabsNavigation() {
-  const { darkMode } = useTheme();
   return (
-    <NavigationContainer
-      theme={{ colors: { background: darkMode ? "black" : "white" } }}
-    >
+    <NavigationContainer>
       <MainTabs.Navigator initialRouteName="Explore">
         <MainTabs.Screen
           name="Explore"
           component={ExploreScreen}
-          options={{ headerShown: false }}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="search" color={color} size={size} />
+            ),
+            tabBarLabel: "Explorar",
+          }}
         />
 
         <MainTabs.Screen
           name="Search"
           component={SearchScreen}
+          options={{ headerShown: false }}
+        />
+        <MainTabs.Screen
+          name="Advertise"
+          component={AdvertiseScreen}
           options={{ headerShown: false }}
         />
       </MainTabs.Navigator>
--- src/screens/main/Advertise.tsx
+++ src/screens/main/Advertise.tsx
@@ -1,1 +1,14 @@
-export default function AdvertiseScreen() {}
+import { Text, View } from "react-native";
+import { MainTabsScreenProps } from "../../navigation/types";
+
+const AdvertiseScreen: React.FC<MainTabsScreenProps<"Explore">> = ({
+  navigation,
+}) => {
+  return (
+    <View className="flex justify-center items-center h-full">
+      <Text> Yay, Advertising</Text>
+    </View>
+  );
+};
+
+export default AdvertiseScreen;
